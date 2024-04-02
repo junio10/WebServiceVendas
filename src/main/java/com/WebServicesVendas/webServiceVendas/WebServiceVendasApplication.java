c8bc7e15fb40dcea56cbdffcbffdc4cbcc136f63
junio10 <junio.js977@gmail.com>
2024-04-01 23:06:56 -0300

    subindo projeto

---
+++ src/main/java/com/WebServicesVendas/webServiceVendas/WebServiceVendasApplication.java
@@ -0,0 +1,13 @@
+package com.WebServicesVendas.webServiceVendas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebServiceVendasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebServiceVendasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/WebServicesVendas/webServiceVendas/WebServiceVendasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.WebServicesVendas.webServiceVendas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebServiceVendasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
