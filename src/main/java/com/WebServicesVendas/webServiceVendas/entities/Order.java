c34e94fcbf7fbc5f530b60f370ff45112c448ea1
junio10 <junio.js977@gmail.com>
2024-05-08 23:41:09 -0300

    adicionando classe payment, e fazendo detalhes oneToMany em product

--- src/main/java/com/WebServicesVendas/webServiceVendas/entities/Order.java
+++ src/main/java/com/WebServicesVendas/webServiceVendas/entities/Order.java
@@ -6,15 +6,20 @@ import java.util.HashSet;
 import java.util.Objects;
 import java.util.Set;
 
+import org.hibernate.annotations.Cascade;
+
 import com.WebServicesVendas.webServiceVendas.entities.enums.OrderStatus;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.MapKey;
 import jakarta.persistence.OneToMany;
+import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 
 @Entity
@@ -34,6 +39,9 @@ public class Order implements Serializable{
 	
 	@OneToMany(mappedBy = "id.order")
 	private Set<OrderItem> items = new HashSet<>();
+	
+	@OneToOne(mappedBy = "order", cascade = CascadeType.ALL)
+	private Payment payment;
 	
 	public Order() {
 		
--- src/main/java/com/WebServicesVendas/webServiceVendas/entities/Product.java
+++ src/main/java/com/WebServicesVendas/webServiceVendas/entities/Product.java
@@ -5,6 +5,8 @@ import java.util.HashSet;
 import java.util.Objects;
 import java.util.Set;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -12,6 +14,7 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.JoinTable;
 import jakarta.persistence.ManyToMany;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 @Entity
@@ -31,6 +34,11 @@ public class Product implements Serializable {
 	joinColumns = @JoinColumn(name="product_id"),
 	inverseJoinColumns = @JoinColumn(name="category_id"))//define qual a outra chave estrangeira
 	private Set<Category> categories = new HashSet<>();
+	
+
+	@OneToMany(mappedBy = "id.product")
+	private Set<OrderItem> items = new HashSet<>();//a diferencao entre o "set" e p list
+	                                                   //e que o set nao admitir repeticoes
 	
 	public Product() {
 		
@@ -76,6 +84,14 @@ public class Product implements Serializable {
 	}
 	public void setImgUrl(String imgUrl) {
 		this.imgUrl = imgUrl;
+	}
+	@JsonIgnore
+	public Set<Order> getOrderProduct() {
+		Set<Order> item = new HashSet<>();
+		for(OrderItem x : items) {
+			item.add(x.getOrder());
+		}
+		return item;
 	}
 	
 
