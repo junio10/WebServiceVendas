c8121a27b29deac979c44f7119e8d788f9b3847b
junio10 <junio.js977@gmail.com>
2024-05-10 23:19:37 -0300

    teste com params no delete de category

--- src/main/java/com/WebServicesVendas/webServiceVendas/resources/CategoryResource.java
+++ src/main/java/com/WebServicesVendas/webServiceVendas/resources/CategoryResource.java
@@ -6,10 +6,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.WebServicesVendas.webServiceVendas.entities.Category;
@@ -31,7 +31,7 @@ public class CategoryResource {
     }
     
     @DeleteMapping(value="/deleteByName")
-    public ResponseEntity<Integer> deleteByName(@PathVariable String name){
+    public ResponseEntity<Integer> deleteByName(@RequestParam String name){
     	Integer isSucess = category.deleteByName(name);
         return ResponseEntity.ok(isSucess);
     }
