8994eca5a03b79f26b32dffb19963ae737d1bf29
junio10 <junio.js977@gmail.com>
2024-08-20 17:52:55 -0300

    feat: adicionando nova consulta com paginação

--- src/main/java/com/WebServicesVendas/webServiceVendas/repositories/OrderItemRepository.java
+++ src/main/java/com/WebServicesVendas/webServiceVendas/repositories/OrderItemRepository.java
@@ -1,9 +1,10 @@
 package com.WebServicesVendas.webServiceVendas.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.WebServicesVendas.webServiceVendas.entities.OrderItem;
 import com.WebServicesVendas.webServiceVendas.entities.pk.OrderItemPK;
-
+@Repository
 public interface OrderItemRepository extends JpaRepository<OrderItem, OrderItemPK>{
 
 }
--- src/main/java/com/WebServicesVendas/webServiceVendas/repositories/OrderRepository.java
+++ src/main/java/com/WebServicesVendas/webServiceVendas/repositories/OrderRepository.java
@@ -3,13 +3,15 @@ package com.WebServicesVendas.webServiceVendas.repositories;
 
 import java.util.List;
 
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
 import com.WebServicesVendas.webServiceVendas.entities.Order;
-
+@Repository
 public interface OrderRepository extends JpaRepository<Order, Long> {
 	@Query("SELECT o FROM Order o WHERE o.user.id = :id")
-	List<Order> findOrderByUser(@Param("id") int id);
+	List<Order> findOrderByUser(@Param("id") int id, Pageable page);
 }
